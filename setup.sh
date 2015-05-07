1472f12259a6abf9782070222e8466ad772c43db
Justin McGuire <jm@landedstar.com>
2015-05-07 16:25:44 -0400

    smarter setup, and removing hard tabs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 ## execute this to setup your environment
 
+## save our current settings
+cat ~/.bash_profile >>~/.bash_local
+
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.bash_fns ~/.bash_fns
@@ -15,4 +18,9 @@ if [ -f ~/.bash_profile ]; then
 fi
 
 EOF
+
+## by default our dotfiles dir appears as a non-hidden dir, lets change that
+if [ -d ~/dotfiles ]; then
+  mv ~/dotfiles ~/.dotfiles
+fi
 

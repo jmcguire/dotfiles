0b6ddc14c39155f62348f9f7d92718ad904ff9db
Justin McGuire <jm@landedstar.com>
2015-09-21 15:57:46 -0400

    added .vim directory with markdown extension
     - right now it only includes the markdown syntax highlighter

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.bash_fns ~/.bash_fns
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim/ ~/.vim/
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.my.gitignore ~/.gitignore
 

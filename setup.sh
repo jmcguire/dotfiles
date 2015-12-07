cf3c0ef31d398202982012aa3acec0fe8b66a498
Justin McGuire <jm@landedstar.com>
2015-12-06 20:41:44 -0500

    revert l, add logging info to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 ## save our current settings
 if [ -f ~/.bash_profile ]; then
+  echo "saving .bash_profile to .bash_local"
   cat ~/.bash_profile >>~/.bash_local
 else
+  echo "creating .bash_local"
   touch ~/.bash_local
 fi
 
+echo "creating dot aliases"
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/.bash_fns ~/.bash_fns
@@ -16,6 +19,7 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.my.gitignore ~/.gitignore
 
 ## setup the .bashrc file
+echo "sourcing .bash_profile in .bashrc"
 cat >> ~/.bashrc <<EOF
 
 ## source my global definitions
@@ -27,6 +31,13 @@ EOF
 
 ## by default our dotfiles dir appears as a non-hidden dir, lets change that
 if [ -d ~/dotfiles ]; then
+  echo "hiding .dotfiles/"
   mv ~/dotfiles ~/.dotfiles
+fi
+
+## setup the bin/ directory
+if [ ! -d ~/bin ]; then
+  echo "creating the bin/ directory"
+  mkdir ~/bin/
 fi
 
